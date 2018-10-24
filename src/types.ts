d8a168416eaf933ffaf2c8aeeef373d1b5cd9e3e
casserni <nicholascassera@gmail.com>
2018-10-24 16:02:49 -0500

    [SL-140] create typings for a theme interface

---
+++ src/types.ts
@@ -0,0 +1,77 @@
+export type FontSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl';
+export type FontWeight = 'thin' | 'light' | 'normal' | 'medium' | 'semibold' | 'bold' | 'extrabold';
+export type LineHeight = 'reset' | 'none' | 'tight' | 'normal' | 'loose';
+export type LetterSpacing = 'tight' | 'normal' | 'wide';
+export type BorderRadius = 'none' | 'sm' | 'md' | 'lg' | 'xl' | 'full';
+export type BorderWidth = 'none' | 'xs' | 'sm' | 'md' | 'lg';
+export type BoxShadow = 'sm' | 'md' | 'lg';
+export type Space =
+  | 'none'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl'
+  | '5xl'
+  | '6xl';
+
+export type FullSpace =
+  | Space
+  | '-xs'
+  | '-sm'
+  | '-md'
+  | '-lg'
+  | '-xl'
+  | '-2xl'
+  | '-3xl'
+  | '-4xl'
+  | '-5xl'
+  | '-6xl';
+
+export interface IThemeInterface<TSections = '', TComponents = ''>
+  extends ISectionTheme<TComponents> {
+  base: ILayout;
+
+  // Extended from ISectionTheme these will be globals used throughout the app
+  // colors?: ...
+  // components?: ...
+
+  // uses generics so you can pass in sections TSections = header | footer | sidebar... (this allows different typings for platform and hubs)
+  // sections are used to colorize the bg/fg/border of the section as well as override nested component colors
+  sections?: { [section in keyof TSections]: ISectionTheme<TComponents> };
+}
+export interface ISectionTheme<TComponents = ''> {
+  colors?: IColors;
+
+  components?: { [component in keyof TComponents]: Partial<IColors> };
+}
+
+export interface ILayout {
+  textSize: { [key in FontSize]?: number | string }; // px
+
+  weight: { [key in FontWeight]?: number };
+
+  leading: { [key in LineHeight]?: number };
+
+  tracking: { [key in LetterSpacing]?: number }; // ems
+
+  radius: { [key in BorderRadius]?: number | string }; // px
+
+  border: { [key in BorderWidth]?: number | string }; // px
+
+  shadow: { [key in BoxShadow]?: string };
+
+  space: { [key in Space]?: number | string }; // px
+}
+
+// Can be nested into itself example { colors: { primary: { base: #FFFF, light: #FFFF } } }
+export interface IColors {
+  fg: string;
+  bg: string;
+  border: string;
+
+  [color: string]: string | Partial<IColors>;
+}
