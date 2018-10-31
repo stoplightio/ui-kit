5c152292263fcc4e6567c0cc9814960d38b7d50f
casserni <nicholascassera@gmail.com>
2018-10-31 10:33:17 -0400

    fix icon story

--- src/components/__stories__/Icon.tsx
+++ src/components/__stories__/Icon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { pick } from 'lodash';
 
 import * as icons from '@fortawesome/pro-solid-svg-icons';
 
@@ -10,54 +11,62 @@ import { withKnobs } from '@storybook/addon-knobs';
 
 import { Icon } from '../Icon';
 
-import { BorderRadius, BorderWidth, FontSize, FullSpace, PositionOpts } from './_utils';
+import { boxKnobs } from './Box';
+
+export const iconKnobs = (tabName = 'Icon') => {
+  return {
+    icon: icons[select('icon', Object.keys(icons), 'faBolt', 'Icon')],
+    spin: boolean('spin', false, 'Icon'),
+    pulse: boolean('pulse', false, 'Icon'),
+    flip: select('flip', [undefined, 'horizontal', 'vertical', 'both'], undefined, 'Icon'),
+    rotation: select(
+      'rotation',
+      { undefined: 'undefined', '90': 90, '180': '180', '270': 270 },
+      'undefined',
+      'Icon'
+    ),
+  };
+};
 
 storiesOf('components/Icon', module)
   .addDecorator(withKnobs)
   .add('with defaults', () => (
     <Icon
-      icon={icons[select('icon', Object.keys(icons), 'faBolt', 'Icon')]}
-      spin={boolean('spin', false, 'Icon')}
-      pulse={boolean('pulse', false, 'Icon')}
-      flip={select('flip', [undefined, 'horizontal', 'vertical', 'both'], undefined, 'Icon')}
-      rotation={select(
-        'rotation',
-        { undefined: 'undefined', '90': 90, '180': '180', '270': 270 },
-        'undefined',
-        'Icon'
-      )}
-      // inherited from box
-      fg={text('fg', undefined, 'Box')}
-      bg={text('bg', undefined, 'Box')}
-      text={select('text', FontSize, undefined, 'Box')}
-      m={select('m', FullSpace, undefined, 'Box')}
-      mt={select('mt', FullSpace, undefined, 'Box')}
-      mr={select('mr', FullSpace, undefined, 'Box')}
-      mb={select('mb', FullSpace, undefined, 'Box')}
-      ml={select('ml', FullSpace, undefined, 'Box')}
-      mx={select('mx', FullSpace, undefined, 'Box')}
-      my={select('my', FullSpace, undefined, 'Box')}
-      p={select('p', FullSpace, undefined, 'Box')}
-      pt={select('pt', FullSpace, undefined, 'Box')}
-      pr={select('pr', FullSpace, undefined, 'Box')}
-      pb={select('pb', FullSpace, undefined, 'Box')}
-      pl={select('pl', FullSpace, undefined, 'Box')}
-      px={select('px', FullSpace, undefined, 'Box')}
-      py={select('py', FullSpace, undefined, 'Box')}
-      border={select('border', BorderWidth, undefined, 'Box')}
-      borderTop={select('borderTop', BorderWidth, undefined, 'Box')}
-      borderLeft={select('borderLeft', BorderWidth, undefined, 'Box')}
-      borderRight={select('borderRight', BorderWidth, undefined, 'Box')}
-      borderBottom={select('borderBottom', BorderWidth, undefined, 'Box')}
-      borderColor={text('borderColor', undefined, 'Box')}
-      radius={select('radius', BorderRadius, undefined, 'Box')}
-      opacity={number('opacity', 1, {}, 'Box')}
-      postion={select('position', PositionOpts, undefined, 'Box')}
-      top={number('top', undefined, {}, 'Box')}
-      bottom={number('bottom', undefined, {}, 'Box')}
-      left={number('left', undefined, {}, 'Box')}
-      right={number('right', undefined, {}, 'Box')}
-      z={number('z', undefined, {}, 'Box')}
+      {...iconKnobs()}
+      // Only supports a subset of box
+      {...pick(boxKnobs(), [
+        'fg',
+        'bg',
+        'text',
+        'm',
+        'mt',
+        'mr',
+        'mb',
+        'ml',
+        'mx',
+        'my',
+        'p',
+        'pt',
+        'pr',
+        'pb',
+        'pl',
+        'px',
+        'py',
+        'border',
+        'borderTop',
+        'borderLeft',
+        'borderRight',
+        'borderBottom',
+        'borderColor',
+        'radius',
+        'opacity',
+        'postion',
+        'top',
+        'bottom',
+        'left',
+        'right',
+        'z',
+      ])}
     >
       Some Text in a P tag
     </Icon>
