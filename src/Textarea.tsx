4b2b8dc5fe79ee399af02daae67ac2b2ac7a853a
Vazha Omanashvili <rainum.ua@gmail.com>
2018-11-05 11:06:21 +0200

    add textarea autosize component and story

---
+++ src/Textarea.tsx
@@ -0,0 +1,47 @@
+import AutosizeTextarea from 'react-textarea-autosize';
+
+import { ITextProps, Text } from './Text';
+import { styled } from './utils';
+
+export interface ITextareaProps extends ITextProps {
+  autosize?: boolean;
+  minRows?: number;
+  maxRows?: number;
+  as?: any;
+}
+
+export const Textarea = styled<ITextareaProps>(Text as any).attrs({
+  as: ({ autosize }: ITextareaProps) => (autosize ? AutosizeTextarea : 'textarea'),
+})(
+  {
+    // @ts-ignore
+    ':focus': {
+      outline: 'none',
+      opacity: 1,
+    },
+  },
+  // disabled style
+  // @ts-ignore
+  props =>
+    props.disabled && {
+      cursor: 'not-allowed',
+      opacity: 0.6,
+    }
+);
+
+Textarea.defaultProps = {
+  display: 'block',
+  weight: 'semibold',
+  align: 'left',
+  m: 'none',
+  px: 'md',
+  py: 'sm',
+  border: 'xs',
+  radius: 'md',
+
+  // reference colors by path in theme
+  // if path does not exist it at component, default to color.fg || color.bg || color.border respectively
+  fg: 'textarea.fg',
+  bg: 'textarea.bg',
+  borderColor: 'textarea.border',
+};
