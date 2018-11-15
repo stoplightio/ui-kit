aca549ea0127fd54108c7795844b77bb0ab33d4c
Jakub Rożek <jakub@stoplight.io>
2018-11-15 12:47:34 +0100

    build(jest): collect coverage from tsx files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: '@stoplight/scripts',
   setupTestFrameworkScriptFile: './setupTests.js',
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"]
 };
