a0ba331f51eae54a1d3e70a912d27e45766d4824
Jakub Rożek <jakub@stoplight.io>
2018-11-16 10:06:50 +0100

    test(menu): add basic tests

---
+++ src/__tests__/Menu.spec.tsx
@@ -0,0 +1,68 @@
+/**
+ * @jest-environment jsdom
+ */
+import { mount, shallow } from 'enzyme';
+import 'jest-enzyme';
+import * as React from 'react';
+import { Icon, IThemeInterface, Menu, MenuItem } from '../';
+import { ThemeProvider } from '../utils';
+
+describe('Menu', () => {
+  it('renders children', () => {
+    const children = <span>test</span>;
+
+    expect(shallow(<Menu>{children}</Menu>)).toContainReact(children);
+  });
+});
+
+describe('MenuItem', () => {
+  it('renders proper Icon', () => {
+    const theme = {
+      base: { space: {} },
+    } as IThemeInterface;
+
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <MenuItem icon="globe" />
+      </ThemeProvider>
+    );
+
+    expect(wrapper.find(Icon)).toExist();
+    expect(wrapper.find(Icon)).toHaveProp('icon', 'globe');
+    wrapper.unmount();
+  });
+
+  it('propagates onClick event', () => {
+    const onClick = jest.fn();
+    const event = { type: 'click' };
+    const wrapper = shallow(<MenuItem onClick={onClick} />);
+
+    wrapper.simulate('click', event);
+
+    expect(onClick).toHaveBeenCalledWith(event);
+  });
+
+  it('renders proper text', () => {
+    const text = <h4>menu entry</h4>;
+    const wrapper = mount(<MenuItem text={text} />);
+
+    expect(wrapper).toContainReact(text);
+    wrapper.unmount();
+  });
+
+  it('renders subtext', () => {
+    const subText = <h4>sub text</h4>;
+    const wrapper = mount(<MenuItem text="text" subText={subText} />);
+
+    expect(wrapper).toContainReact(subText);
+    wrapper.unmount();
+  });
+
+  it('does not render subtext only if is missing', () => {
+    const subText = <h4>test</h4>;
+    const wrapper = mount(<MenuItem subText={subText} />);
+
+    expect(wrapper).not.toContainReact(subText);
+    wrapper.unmount();
+  });
+});
