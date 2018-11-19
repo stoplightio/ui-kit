ded70930c1dff2701c0cf038c5991397b9eb7f8a
Jakub Rożek <jakub@stoplight.io>
2018-11-19 17:02:23 +0100

    refactor(code-editor): scoped styles

--- src/CodeEditor.tsx
+++ src/CodeEditor.tsx
@@ -3,94 +3,8 @@ import { highlight, languages } from 'prismjs';
 import 'prismjs/components/';
 import * as React from 'react';
 import Editor from 'react-simple-code-editor';
-import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import { themeGet } from 'styled-system';
-
-const CodeEditorStyle = createGlobalStyle`
-  .ui-kit-code-editor {
-    background: ${themeGet('components.codeEditor.bg', '#fff')};
-    border: 1px solid ${themeGet('components.codeEditor.border', '#000')};
-    
-    textarea {
-      &:focus {
-        outline: none;
-      }
-    }
-    
-    .namespace {
-      opacity: 0.7;
-    }
-      
-    .token {
-      &.comment,
-      &.prolog,
-      &.doctype,
-      &.cdata {
-        color: ${themeGet('components.codeEditor.syntax.comment', '#90a4ae')};
-      }
-    
-      &.punctuation {
-        color: #9e9e9e;
-      }
-      
-      &.property,
-      &.tag,
-      &.boolean,
-      &.number,
-      &.symbol,
-      &.deleted {
-        color: ${themeGet('components.codeEditor.syntax.primary', '#e91e63')};
-      }
-      
-      &.selector,
-      &.attr-name,
-      &.string,
-      &.char,
-      &.builtin,
-      &.inserted {
-        color: ${themeGet('components.codeEditor.syntax.secondary', '#4caf50')};
-      }
-
-      &.operator,
-      &.entity,
-      &.url {
-        color: ${themeGet('components.codeEditor.syntax.operator', '#795548')};
-      }
-      
-      &.atrule,
-      &.attr-value,
-      &.keyword {
-        color: ${themeGet('components.codeEditor.syntax.keyword', '#3f51b5')};
-      }
-      
-      &.function {
-        color: ${themeGet('components.codeEditor.syntax.function', '#f44336')};
-      }
-      
-      &.variable {
-        color: ${themeGet('components.codeEditor.syntax.variable', '#ff9800')};
-      }
-
-      &.regex,
-      &.important {
-        color: ${themeGet('components.codeEditor.syntax.regex', '#ff9800')};
-      }
-
-      &.important,
-      &.bold {
-        font-weight: bold;
-      }
-    
-      &.italic {
-        font-style: italic;
-      }
-    
-      &.entity {
-        cursor: help;
-      }
-    }
-  }
-`;
 
 export interface ICodeEditorProps {
   code?: string;
@@ -99,8 +13,9 @@ export interface ICodeEditorProps {
   style?: object;
 }
 
-export const CodeEditor = (props: ICodeEditorProps) => {
-  const { language, onChange = noop, style } = props;
+// @ts-ignore
+export const CodeEditor = styled<ICodeEditorProps, 'div'>((props: ICodeEditorProps & { className: string }) => {
+  const { className, language, onChange = noop, style } = props;
 
   const [value, setValue] = React.useState(props.code || '');
 
@@ -110,8 +25,7 @@ export const CodeEditor = (props: ICodeEditorProps) => {
   };
 
   return (
-    <div className="ui-kit-code-editor">
-      <CodeEditorStyle />
+    <div className={className}>
       <Editor
         value={value}
         onValueChange={handleChange}
@@ -121,4 +35,86 @@ export const CodeEditor = (props: ICodeEditorProps) => {
       />
     </div>
   );
-};
+})`
+  background: ${themeGet('components.codeEditor.bg', '#fff')};
+  border: 1px solid ${themeGet('components.codeEditor.border', '#000')};
+
+  textarea {
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .namespace {
+    opacity: 0.7;
+  }
+
+  .token {
+    &.comment,
+    &.prolog,
+    &.doctype,
+    &.cdata {
+      color: ${themeGet('components.codeEditor.syntax.comment', '#90a4ae')};
+    }
+
+    &.punctuation {
+      color: #9e9e9e;
+    }
+
+    &.property,
+    &.tag,
+    &.boolean,
+    &.number,
+    &.symbol,
+    &.deleted {
+      color: ${themeGet('components.codeEditor.syntax.primary', '#e91e63')};
+    }
+
+    &.selector,
+    &.attr-name,
+    &.string,
+    &.char,
+    &.builtin,
+    &.inserted {
+      color: ${themeGet('components.codeEditor.syntax.secondary', '#4caf50')};
+    }
+
+    &.operator,
+    &.entity,
+    &.url {
+      color: ${themeGet('components.codeEditor.syntax.operator', '#795548')};
+    }
+
+    &.atrule,
+    &.attr-value,
+    &.keyword {
+      color: ${themeGet('components.codeEditor.syntax.keyword', '#3f51b5')};
+    }
+
+    &.function {
+      color: ${themeGet('components.codeEditor.syntax.function', '#f44336')};
+    }
+
+    &.variable {
+      color: ${themeGet('components.codeEditor.syntax.variable', '#ff9800')};
+    }
+
+    &.regex,
+    &.important {
+      color: ${themeGet('components.codeEditor.syntax.regex', '#ff9800')};
+    }
+
+    &.important,
+    &.bold {
+      font-weight: bold;
+    }
+
+    &.italic {
+      font-style: italic;
+    }
+
+    &.entity {
+      cursor: help;
+    }
+  }
+`;
