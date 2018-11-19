05f4a4bac95caace3cd93226bc247748e858fe8e
Jakub Rożek <jakub@stoplight.io>
2018-11-19 19:21:10 +0100

    test(code-editor): add tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@stoplight/scripts',
   setupTestFrameworkScriptFile: './setupTests.js',
-  snapshotSerializers: ['enzyme-to-json/serializer']
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testEnvironment: 'jsdom'
 };
---
+++ src/__tests__/CodeEditor.spec.tsx
@@ -0,0 +1,42 @@
+import { mount } from 'enzyme';
+import 'jest-enzyme';
+import * as React from 'react';
+import Editor from 'react-simple-code-editor';
+import { CodeEditor } from '../CodeEditor';
+
+describe('CodeEditor', () => {
+  let useStateSpy: jest.SpyInstance;
+  let setValueMock: jest.Mock;
+
+  beforeEach(() => {
+    useStateSpy = jest.spyOn(React, 'useState');
+
+    setValueMock = jest.fn();
+    useStateSpy.mockReturnValueOnce(['', setValueMock]);
+  });
+
+  afterEach(() => {
+    useStateSpy.mockRestore();
+  });
+
+  it('calls onChange on code change', () => {
+    const onChange = jest.fn();
+    const newCode = 'abcdef';
+
+    const wrapper = mount(<CodeEditor onChange={onChange} language="js" />);
+
+    wrapper.find(Editor).prop('onValueChange')(newCode);
+    expect(onChange).toHaveBeenCalledWith(newCode);
+    wrapper.unmount();
+  });
+
+  it('update value on code change', () => {
+    const newCode = 'abcdef';
+
+    const wrapper = mount(<CodeEditor onChange={() => null} language="js" />);
+
+    wrapper.find(Editor).prop('onValueChange')(newCode);
+    expect(setValueMock).toHaveBeenCalledWith(newCode);
+    wrapper.unmount();
+  });
+});
