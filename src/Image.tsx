40ffd9b228284258f18e0aa28071bd283237e741
Jakub Rożek <jakub@stoplight.io>
2018-11-28 15:22:27 +0100

    refactor: make Image more flexible

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,55 +1,31 @@
-import * as React from 'react';
+import { styled } from './utils';
 
-import { borderRadius, height, opacity, styled, width } from './utils';
+import { Box, IBoxProps } from './Box';
 
-import { BorderRadius } from './types';
-
-export interface IImageProps {
-  src: string;
-  label?: string;
-  radius?: BorderRadius;
+export interface IImageProps extends IBoxProps {
+  src: HTMLImageElement['src'];
+  alt?: HTMLImageElement['alt'];
+  title?: HTMLImageElement['title'];
   hidden?: boolean;
-  opacity?: number;
   responsive?: boolean;
-  height?: string;
-  width?: string;
+  height?: HTMLImageElement['height'];
+  width?: HTMLImageElement['width'];
 }
 
-interface IPlainImageProps extends IImageProps {
-  className: string;
-}
-
-const PlainImage = (props: IPlainImageProps) => <img className={props.className} src={props.src} title={props.label} />;
-
-export const Image = styled<IImageProps, 'img'>(PlainImage as any)(
-  {
-    // @ts-ignore
-    display: 'inline',
-  },
-
-  borderRadius,
-  opacity,
-  width,
-  height,
-
-  ({ hidden }: IImageProps) =>
-    hidden && {
-      display: 'none',
-    },
-
-  ({ responsive }: IImageProps) =>
-    responsive && {
-      maxWidth: '100%',
-    }
-);
+export const Image = styled<IImageProps, 'img'>(Box).attrs({
+  as: 'img',
+})`
+  ${({ hidden }: IImageProps) => hidden && `display: none;`}
+   
+  ${({ responsive }: IImageProps) => responsive && `
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+  `};
+`;
 
 Image.defaultProps = {
-  circular: false,
-  label: '',
-  height: 'auto',
   hidden: false,
-  opacity: 1,
-  responsive: true,
-  rounded: false,
-  width: 'auto',
+  responsive: false,
 };
