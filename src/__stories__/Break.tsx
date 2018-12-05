f62d082745af4ce4b7b36e61c16baca5787a980b
Jakub Rożek <jakub@stoplight.io>
2018-12-05 11:11:26 +0100

    feat: convert Break

--- src/__stories__/Break.tsx
+++ src/__stories__/Break.tsx
@@ -5,7 +5,7 @@ import { number } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
 import omitBy = require('lodash/omitBy');
 
-import { Break } from '../Break';
+import { Break } from '../emotion/Break';
 
 export const breakKnobs = (tabName = 'Break'): any => {
   return omitBy(
