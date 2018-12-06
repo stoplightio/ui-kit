f61b1b513dc9faed98659d8c17c321f9321be910
Jakub Rożek <jakub@stoplight.io>
2018-12-06 09:30:05 +0100

    feat: re-add Portal + index.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+/**
+ * NOTE: any components with meaningful external dependencies should NOT be exported here.
+ * Library users will need to `import { Select } from '@stoplight/ui-kit/select` them specifically.
+ */
+
+// export * from './BlockQuote';
+export * from './Break';
+export * from './Button';
+// export * from './ContextMenu';
+export * from './Flex';
+export * from './Heading';
+export * from './Icon';
+export * from './Image';
+// export * from './List';
+export * from './Link';
+// export * from './Popup';
+export * from './Portal';
+// export * from './Mark';
+// export * from './Menu';
+// export * from './Table';
+export * from './Text';
+// export * from './Toggle';
+export * from './theme/types';
