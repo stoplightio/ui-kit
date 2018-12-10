e256a7706b2dee1166c26a86291c5e5b972af577
Jakub Rożek <jakub@stoplight.io>
2018-12-10 13:48:00 +0100

    chore: remove __old__

--- __old__/utils/highlightCode.ts
+++
@@ -1,6 +0,0 @@
-import { highlight, languages } from 'prismjs';
-
-export const highlightCode = (code: string, language: string) => {
-  const langDef = languages[language];
-  return langDef ? highlight(code, langDef) : code;
-};
