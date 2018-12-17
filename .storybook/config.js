af16b7927187d47736b063eb44f06c12d9f8f524
casserni <nicholascassera@gmail.com>
2018-12-17 09:02:24 -0500

    fix(import): removes double slash from import

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { withInfo } from '@storybook/addon-info';
 import { withOptions } from '@storybook/addon-options';
 import { addDecorator, configure } from '@storybook/react';
 
-import * as themes from './custom-addons/withThemes//themes';
+import * as themes from './custom-addons/withThemes/themes';
 import { withThemes } from './custom-addons/withThemes/decorator';
 
 addDecorator(
@@ -39,36 +39,40 @@ addDecorator(
   })
 );
 
-addDecorator(withThemes(themes, {
-  app: ({ base }) => ({
-    canvas: base === 'light'
-      ? {
-        fg: '#111',
-        bg: '#fff',
-      }
-      : {
-        fg: '#fff',
-        bg: '#111'
-      }
-  }),
-  inverted: ({ base }) => ({
-    container: base === 'dark'
-        ? {
-        fg: '#111',
-        bg: '#fff',
-      }
-      : {
-        fg: '#fff',
-        bg: '#111'
-      }
-  }),
-  inner: {
-    container: {
-      fg: 'white',
-      bg: 'purple',
+addDecorator(
+  withThemes(themes, {
+    app: ({ base }) => ({
+      canvas:
+        base === 'light'
+          ? {
+              fg: '#111',
+              bg: '#fff',
+            }
+          : {
+              fg: '#fff',
+              bg: '#111',
+            },
+    }),
+    inverted: ({ base }) => ({
+      container:
+        base === 'dark'
+          ? {
+              fg: '#111',
+              bg: '#fff',
+            }
+          : {
+              fg: '#fff',
+              bg: '#111',
+            },
+    }),
+    inner: {
+      container: {
+        fg: 'white',
+        bg: 'purple',
+      },
     },
-  },
-}));
+  })
+);
 
 function loadStories() {
   require('../src/__stories__');
