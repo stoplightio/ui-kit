24627fa7c24a8ccd6ec23e5b890e1fa849639b98
William Hilton <wmhilton@gmail.com>
2019-02-22 10:39:35 -0500

    chore: organize code slightly

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -2,6 +2,8 @@
 import { jsx } from '@emotion/core';
 import * as React from 'react';
 
+const capitalize = require('lodash/capitalize');
+
 import { Box, IBox, IBoxCSS } from './Box';
 
 import { Flex } from './Flex';
@@ -38,20 +40,11 @@ const tooltipStyles = ({ invalid, posX = 'left', posY = 'top' }: ITooltip): IBox
   const theme = { ...baseTheme };
   if (invalid) Object.assign(theme, invalidTheme);
 
-  const sz = (20 * Math.SQRT2) / 2;
-  let margin = '';
-  if (posY === 'bottom') {
-    margin = `${sz}px 0 0 0`;
-  } else if (posY === 'top') {
-    margin = `0 0 ${sz}px 0`;
-  } else if (posY === 'center') {
-    if (posX === 'right') {
-      margin = `0 0 0 ${sz}px`;
-    } else if (posX === 'center') {
-      margin = `0 0 0 0`;
-    } else if (posX === 'left') {
-      margin = `0 ${sz}px 0 0`;
-    }
+  const margin: React.CSSProperties = {};
+  if (posY !== 'center') {
+    margin[opposingMargin(posY)] = `${caretHeight}px`;
+  } else if (posY === 'center' && posX !== 'center') {
+    margin[opposingMargin(posX)] = `${caretHeight}px`;
   }
   return [
     {
@@ -60,7 +53,8 @@ const tooltipStyles = ({ invalid, posX = 'left', posY = 'top' }: ITooltip): IBox
       border: `1px solid ${theme.border || theme.fg}`,
       position: 'relative',
       borderRadius: '3px',
-      margin,
+      margin: 0,
+      ...margin,
     },
   ];
 };
@@ -99,8 +93,6 @@ const caretStyles = ({ invalid, posX = 'left', posY = 'top' }: ITooltip): React.
   if (invalid) Object.assign(theme, invalidTheme);
 
   const pstyles: React.CSSProperties = {};
-  const sz = 20; // length of caret's side
-  const b = 1; // border width
 
   pstyles.width = `${sz}px`;
   pstyles.height = `${sz}px`;
@@ -108,67 +100,99 @@ const caretStyles = ({ invalid, posX = 'left', posY = 'top' }: ITooltip): React.
   pstyles.backgroundColor = theme.bg;
   if (posY === 'bottom') {
     if (posX === 'right') {
-      pstyles.transformOrigin = 'top right';
-      pstyles.transform = 'rotate(45deg)';
-      pstyles.borderBottomWidth = 0;
-      pstyles.borderRightWidth = 0;
+      caretCorner(pstyles, 'top', 'left');
+      rotate(pstyles, 'top', 'right');
       pstyles.top = `-${b}px`;
       pstyles.right = `${sz}px`;
     } else if (posX === 'center') {
-      pstyles.transform = 'rotate(45deg)';
-      pstyles.borderBottomWidth = 0;
-      pstyles.borderRightWidth = 0;
-      pstyles.top = `-${sz / 2 + b}px`;
-      pstyles.left = `calc(50% - ${(sz * Math.SQRT2) / 2}px)`;
+      caretCorner(pstyles, 'top', 'left');
+      rotateCenterCenter(pstyles);
+      pstyles.top = `-${b + sz / 2}px`;
+      pstyles.left = `calc(50% - ${caretHeight}px)`;
     } else if (posX === 'left') {
-      pstyles.transformOrigin = 'top left';
-      pstyles.transform = 'rotate(-45deg)';
-      pstyles.borderBottomWidth = 0;
-      pstyles.borderLeftWidth = 0;
+      caretCorner(pstyles, 'top', 'right');
+      rotate(pstyles, 'top', 'left');
       pstyles.top = `-${b}px`;
       pstyles.left = `${sz}px`;
     }
   } else if (posY === 'top') {
     if (posX === 'right') {
-      pstyles.transformOrigin = 'bottom right';
-      pstyles.transform = 'rotate(-45deg)';
-      pstyles.borderTopWidth = 0;
-      pstyles.borderRightWidth = 0;
+      caretCorner(pstyles, 'bottom', 'left');
+      rotate(pstyles, 'bottom', 'right');
       pstyles.bottom = `-${b}px`;
       pstyles.right = `${sz}px`;
     } else if (posX === 'center') {
-      pstyles.transform = 'rotate(45deg)';
-      pstyles.borderTopWidth = 0;
-      pstyles.borderLeftWidth = 0;
-      pstyles.bottom = `-${sz / 2 + b}px`;
-      pstyles.left = `calc(50% - ${(sz * Math.SQRT2) / 2}px)`;
+      caretCorner(pstyles, 'bottom', 'right');
+      rotateCenterCenter(pstyles);
+      pstyles.bottom = `-${b + sz / 2}px`;
+      pstyles.left = `calc(50% - ${caretHeight}px)`;
     } else if (posX === 'left') {
-      pstyles.transformOrigin = 'bottom left';
-      pstyles.transform = 'rotate(45deg)';
-      pstyles.borderTopWidth = 0;
-      pstyles.borderLeftWidth = 0;
+      caretCorner(pstyles, 'bottom', 'right');
+      rotate(pstyles, 'bottom', 'left');
       pstyles.bottom = `-${b}px`;
       pstyles.left = `${sz}px`;
     }
   } else if (posY === 'center') {
     if (posX === 'right') {
-      pstyles.transformOrigin = 'top left';
-      pstyles.transform = 'rotate(-45deg)';
-      pstyles.borderRightWidth = 0;
-      pstyles.borderBottomWidth = 0;
-      pstyles.left = `-${(sz * Math.SQRT2) / 2 + b}px`;
+      caretCorner(pstyles, 'top', 'left');
+      rotate(pstyles, 'top', 'left');
+      pstyles.left = `-${b + caretHeight}px`;
       pstyles.top = '50%';
     } else if (posX === 'center') {
       pstyles.visibility = 'hidden';
     } else if (posX === 'left') {
-      pstyles.transformOrigin = 'top right';
-      pstyles.transform = 'rotate(45deg)';
-      pstyles.borderLeftWidth = 0;
-      pstyles.borderBottomWidth = 0;
-      pstyles.right = `-${(sz * Math.SQRT2) / 2 + b}px`;
+      caretCorner(pstyles, 'top', 'right');
+      rotate(pstyles, 'top', 'right');
+      pstyles.right = `-${b + caretHeight}px`;
       pstyles.top = '50%';
     }
   }
 
   return pstyles;
+};
+
+// CONSTANTS
+const b = 1; // border width
+const sz = 20; // length of caret's side
+const caretHeight = (sz * Math.SQRT2) / 2;
+
+// HELPERS
+type Side = 'center' | 'left' | 'right' | 'top' | 'bottom';
+
+const oppositeSide = (side: Side): Side => {
+  switch (side) {
+    case 'center':
+      return 'center';
+    case 'left':
+      return 'right';
+    case 'right':
+      return 'left';
+    case 'top':
+      return 'bottom';
+    case 'bottom':
+      return 'top';
+  }
+};
+
+const opposingMargin = (side: Side) => 'margin' + capitalize(oppositeSide(side));
+
+const opposingBorderWidth = (side: Side) => 'border' + capitalize(oppositeSide(side)) + 'Width';
+
+const rotate = (styles: React.CSSProperties, side1: 'top' | 'bottom', side2: 'right' | 'left') => {
+  styles.transformOrigin = `${side1} ${side2}`;
+  const signs = [[-1, 1], [1, -1]];
+  const sign = signs[side1 === 'top' ? 0 : 1][side2 === 'left' ? 0 : 1];
+  const deg = 45 * sign;
+  styles.transform = `rotate(${deg}deg)`;
+};
+
+const rotateCenterCenter = (styles: React.CSSProperties) => {
+  styles.transform = 'rotate(45deg)';
+};
+
+// This helper removes the border from two sides of the caret depending on which
+// corner is the caret's tip.
+const caretCorner = (styles: React.CSSProperties, side1: Side, side2: Side) => {
+  styles[opposingBorderWidth(side1)] = 0;
+  styles[opposingBorderWidth(side2)] = 0;
 };
