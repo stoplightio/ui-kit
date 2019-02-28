5dcc850a482be8d656bfb030eb682d0ea00ad581
Chris Miaskowski <chrism@stoplight.io>
2019-02-28 12:57:23 +0100

    refactor: simple test cleanup
    SL-1694

--- src/__tests__/Toast.spec.tsx
+++ src/__tests__/Toast.spec.tsx
@@ -23,12 +23,38 @@ function expectToast(type: string, iconClassName: string) {
 }
 
 describe('Toast', () => {
-  it('renders toasts with icons', () => {
+  it('renders error toast', () => {
+    const { fas } = _solidIcons;
+    IconLibrary.add(fas);
+
+    expectToast('error', 'fa-times-circle');
+  });
+
+  it('renders info toast', () => {
     const { fas } = _solidIcons;
     IconLibrary.add(fas);
 
     expectToast('info', 'fa-info-circle');
+  });
+
+  it('renders default toast', () => {
+    const { fas } = _solidIcons;
+    IconLibrary.add(fas);
+
+    expectToast('default', 'fa-lightbulb');
+  });
+
+  it('renders success toast', () => {
+    const { fas } = _solidIcons;
+    IconLibrary.add(fas);
+
+    expectToast('success', 'fa-check-circle');
+  });
+
+  it('renders warning toast', () => {
+    const { fas } = _solidIcons;
+    IconLibrary.add(fas);
+
     expectToast('warning', 'fa-exclamation-triangle');
-    expectToast('error', 'fa-times-circle');
   });
 });
