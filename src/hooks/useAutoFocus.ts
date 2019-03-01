9ee03aa2e31e0e48b103f362184d0b29a9987a3b
Jakub Rożek <jakub@stoplight.io>
2019-03-02 00:16:16 +0700

    chore: comment the code

--- src/hooks/useAutoFocus.ts
+++ src/hooks/useAutoFocus.ts
@@ -4,6 +4,17 @@ export const useAutoFocus = <T extends Partial<{ focus: () => any }>>(autoFocus?
   const nodeRef = useRef<T>(null);
 
   useEffect(() => {
+    /**
+     * This is needed for ref forwarding
+     *
+     * const YourComponent = React.forwardRef<HTMLInputElement, any>((props, ref) => {
+     *   const [nodeRef] = useAutoFocus(props.autoFocus, ref);
+     *
+     *   return <input ref={nodeRef} value="foo" />;
+     * });
+     *
+     * We also support a ref function (a bit older approach, pre createRef / forwardRef / useRef)
+     */
     if (!ref) return;
     if (typeof ref === 'function') {
       ref(nodeRef.current);
