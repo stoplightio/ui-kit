7282ed32e3cb8e6a96b46ec2a57a7faf3cba1354
Jakub Rożek <jakub@stoplight.io>
2019-03-01 12:39:54 +0700

    refactor: allow css to be passed in

--- src/ScrollList.tsx
+++ src/ScrollList.tsx
@@ -1,9 +1,10 @@
 /* @jsx jsx */
-import { css, jsx } from '@emotion/core';
+import { css as cssx, jsx } from '@emotion/core';
 import { forwardRef } from 'react';
 import * as ReactWindow from 'react-window';
 
 import { AutoSizer } from './AutoSizer';
+import { IBoxCSS } from './Box';
 import { ITheme, useTheme } from './theme';
 
 export {
@@ -12,13 +13,19 @@ export {
   ListItemKeySelector,
   FixedSizeList as IFixedSizeList,
   VariableSizeList as IVariableSizeList,
-  FixedSizeListProps as IFixedSizeListProps,
-  VariableSizeListProps as IVariableSizeListProps,
 } from 'react-window';
 
-export const FixedSizeList = forwardRef<ReactWindow.FixedSizeList, ReactWindow.FixedSizeListProps>((props, ref) => {
+export interface IFixedSizeListProps extends ReactWindow.FixedSizeListProps {
+  css?: IBoxCSS;
+}
+
+export interface IVariableSizeListProps extends ReactWindow.VariableSizeListProps {
+  css?: IBoxCSS;
+}
+
+export const FixedSizeList = forwardRef<ReactWindow.FixedSizeList, IFixedSizeListProps>((props, ref) => {
   const { scrollbar: theme } = useTheme();
-  const { width, height, ...rest } = props;
+  const { width, height, css, ...rest } = props;
 
   return (
     <AutoSizer width={width} height={height}>
@@ -26,7 +33,7 @@ export const FixedSizeList = forwardRef<ReactWindow.FixedSizeList, ReactWindow.F
         <ReactWindow.FixedSizeList
           {...rest}
           ref={ref}
-          css={scrollListStyles(theme)}
+          css={scrollListStyles(theme, css)}
           height={listHeight}
           width={listWidth}
         />
@@ -35,41 +42,42 @@ export const FixedSizeList = forwardRef<ReactWindow.FixedSizeList, ReactWindow.F
   );
 });
 
-export const VariableSizeList = forwardRef<ReactWindow.VariableSizeList, ReactWindow.VariableSizeListProps>(
-  (props, ref) => {
-    const { width, height, ...rest } = props;
-    const { scrollbar: theme } = useTheme();
+export const VariableSizeList = forwardRef<ReactWindow.VariableSizeList, IVariableSizeListProps>((props, ref) => {
+  const { width, height, css, ...rest } = props;
+  const { scrollbar: theme } = useTheme();
 
-    return (
-      <AutoSizer width={width} height={height}>
-        {({ width: listWidth, height: listHeight }) => (
-          <ReactWindow.VariableSizeList
-            {...rest}
-            ref={ref}
-            css={scrollListStyles(theme)}
-            height={listHeight}
-            width={listWidth}
-          />
-        )}
-      </AutoSizer>
-    );
-  }
-);
+  return (
+    <AutoSizer width={width} height={height}>
+      {({ width: listWidth, height: listHeight }) => (
+        <ReactWindow.VariableSizeList
+          {...rest}
+          ref={ref}
+          css={scrollListStyles(theme, css)}
+          height={listHeight}
+          width={listWidth}
+        />
+      )}
+    </AutoSizer>
+  );
+});
 
-export const scrollListStyles = (theme: ITheme['scrollbar']) => {
-  return css`
-    &::-webkit-scrollbar {
-      height: 6px;
-      background-color: transparent;
-      width: 6px;
-    }
+export const scrollListStyles = (theme: ITheme['scrollbar'], css?: IBoxCSS) => {
+  return [
+    cssx`
+      &::-webkit-scrollbar {
+        height: 6px;
+        background-color: transparent;
+        width: 6px;
+      }
 
-    &::-webkit-scrollbar-thumb {
-      background-color: ${theme.bg};
-      border-radius: 10px;
-    }
+      &::-webkit-scrollbar-thumb {
+        background-color: ${theme.bg};
+        border-radius: 10px;
+      }
 
-    scrollbar-color: ${theme.bg} transparent;
-    scrollbar-width: thin;
-  `;
+      scrollbar-color: ${theme.bg} transparent;
+      scrollbar-width: thin;
+    `,
+    css,
+  ];
 };
