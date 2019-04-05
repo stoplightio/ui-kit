abd24e58d265a9398529dc78b076789bd54ed1a4
casserni <nicholascassera@gmail.com>
2019-04-05 10:09:34 -0500

    feat: update autosizer

--- src/AutoSizer.tsx
+++ src/AutoSizer.tsx
@@ -1,21 +1,26 @@
-// import { Omit } from '@stoplight/types';
-// import * as React from 'react';
-// import useResizeObserver from 'use-resize-observer';
-// import { Box, IBox } from './Box';
+import { Omit } from '@stoplight/types';
+import * as React from 'react';
+import useResizeObserver from 'use-resize-observer';
 
-// export interface IAutoSizer extends Omit<IBox, 'children'> {
-//   children: ({ width, height }: { width: React.ReactText; height: React.ReactText }) => React.ReactNode;
-// }
+export interface IAutoSizer extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+  children: ({ width, height }: { width: React.ReactText; height: React.ReactText }) => React.ReactNode;
+}
 
-// const AutoSizer: React.FunctionComponent<IAutoSizer> = props => {
-//   const { children, ...rest } = props;
-//   const [ref, width = '100%', height = '100%'] = useResizeObserver();
+const AutoSizer: React.FunctionComponent<IAutoSizer> = props => {
+  const { children, ...rest } = props;
 
-//   return (
-//     <Box {...rest} ref={ref}>
-//       {children({ width, height })}
-//     </Box>
-//   );
-// };
+  // useResizeObserver does not use generics and the typings aren't complete
+  const [ref, width = '100%', height = '100%'] = useResizeObserver() as [
+    React.RefObject<HTMLDivElement>,
+    React.ReactText,
+    React.ReactText
+  ];
 
-// export { AutoSizer };
+  return (
+    <div {...rest} ref={ref}>
+      {children({ width, height })}
+    </div>
+  );
+};
+
+export { AutoSizer };
