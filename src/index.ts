60c23b3b8370d0cd22046934721c19f8fdc36164
casserni <nicholascassera@gmail.com>
2019-04-19 17:26:16 -0400

    chore: import specif blueprint consts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 // export some helpful blueprint enums and conts
-// TODO export specifics
-export * from '@blueprintjs/core/src/common/alignment';
-export * from '@blueprintjs/core/src/common/boundary';
-export * from '@blueprintjs/core/src/common/colors';
-export * from '@blueprintjs/core/src/common/constructor';
-export * from '@blueprintjs/core/src/common/elevation';
-export * from '@blueprintjs/core/src/common/intent';
-export * from '@blueprintjs/core/src/common/position';
-export * from '@blueprintjs/core/src/common/props';
+export { Alignment, Boundary, Colors, Elevation, Intent, IIntentProps, Position } from '@blueprintjs/core';
 
 // export our components
 export * from './components';
