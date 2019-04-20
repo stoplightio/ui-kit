5bbfb3a4280994a04b9844e824ec66b53f235272
Marc MacLeod <marbemac@gmail.com>
2019-04-19 21:11:46 -0500

    refactor: index.tsx convention
    like we have elsewhere in our codebases

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,3 +1,3 @@
-export * from './FormError/FormError';
-export * from './FormGroup/FormGroup';
-export * from './FormInput/FormInput';
+export * from './FormError';
+export * from './FormGroup';
+export * from './FormInput';
