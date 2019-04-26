8aefa2d48bf4e0dd9927dcec045f1664f3460712
Chris <chrisnlott@gmail.com>
2019-04-25 22:17:30 -0500

    fix: code editor syntax highlighting

--- src/CodeEditor/utils/highlightCode.ts
+++ src/CodeEditor/utils/highlightCode.ts
@@ -1,6 +1,14 @@
-import { highlight, languages } from 'prismjs';
+import * as Prism from 'prismjs';
+
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-yaml';
 
 export const highlightCode = (code: string, language: string) => {
-  const langDef = languages[language];
-  return langDef ? highlight(code, langDef, '') : code;
+  const langDef = Prism.languages[language];
+  return langDef ? Prism.highlight(code, langDef, '') : code;
 };
