1745b0eaa34ae321091a46f8babb5af46b15e650
casserni <nicholascassera@gmail.com>
2019-04-28 23:10:08 -0500

    fix: hide scroll bars in virtualized list

--- src/ScrollList/index.tsx
+++ src/ScrollList/index.tsx
@@ -2,31 +2,72 @@ import * as cn from 'classnames';
 import * as React from 'react';
 import * as ReactWindow from 'react-window';
 
-import { AutoSizer } from '../index';
+import { AutoSizer, Classes } from '../index';
 
 /**
  * FIXED SIZE LIST
  */
 interface IFixedSizeListProps extends ReactWindow.FixedSizeListProps {
   className?: string;
+  autoHideTimeout?: number;
 }
 
 const FixedSizeList: React.FunctionComponent<IFixedSizeListProps> = React.forwardRef<
   ReactWindow.FixedSizeList,
   IFixedSizeListProps
 >(function FixedSizeList(props, ref) {
-  const { className, ...rest } = props;
+  const { className, autoHideTimeout = 500, ...rest } = props;
 
+  const [scrollOffset, setScrollOffset] = React.useState<null | number>(null);
+  const [scrollHeight, setScrollHeight] = React.useState<null | number>(null);
+  const [isScrolling, setIsScrolling] = React.useState<null | number | NodeJS.Timer>(null);
+
+  const outerRef = React.useRef(null);
+
+  // wrap in outer div to hide native scrollbars
   return (
     <AutoSizer>
       {({ width: listWidth, height: listHeight }) => (
-        <ReactWindow.FixedSizeList
-          {...rest}
-          className={cn(className)}
-          ref={ref}
-          height={listHeight}
-          width={listWidth}
-        />
+        <div className="relative overflow-hidden" style={{ height: listHeight, width: listWidth }}>
+          <ReactWindow.FixedSizeList
+            {...rest}
+            className={cn(className)}
+            style={{ marginRight: -15 }}
+            height={listHeight}
+            width={listWidth + 15}
+            outerRef={outerRef}
+            onScroll={() => {
+              if (outerRef.current) {
+                // @ts-ignore says current can be null despite the check
+                setScrollHeight(outerRef.current.scrollWidth);
+                // @ts-ignore
+                setScrollOffset(outerRef.current.scrollTop);
+              }
+
+              if (isScrolling !== null) {
+                clearTimeout(isScrolling as number);
+              }
+
+              setIsScrolling(
+                setTimeout(() => {
+                  setIsScrolling(null);
+                }, autoHideTimeout)
+              );
+            }}
+          />
+          <div className={cn(Classes.SCROLL_TRACK, 'vertical')} style={{ width: 6 }}>
+            <div
+              className={cn(Classes.SCROLL_THUMB, !isScrolling && 'static')}
+              style={{
+                display: 'block',
+                position: 'relative',
+                height: scrollHeight ? scrollHeight - 12 : 0,
+                width: '6px',
+                transform: `translateY(${scrollOffset}px)`,
+              }}
+            />
+          </div>
+        </div>
       )}
     </AutoSizer>
   );
@@ -54,8 +95,9 @@ const VariableSizeList: React.FunctionComponent<IVariableSizeListProps> = React.
           {...rest}
           ref={ref}
           className={cn(className)}
+          style={{ marginRight: -15 }}
           height={listHeight}
-          width={listWidth}
+          width={listWidth + 15}
         />
       )}
     </AutoSizer>
