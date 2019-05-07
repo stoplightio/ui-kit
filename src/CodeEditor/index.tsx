e6e23cd8ed58ae8204f997f90bd12b0309eebd6d
Chris <chrisnlott@gmail.com>
2019-05-07 16:50:52 -0500

    fix: don't wrap the code editor

--- src/CodeEditor/index.tsx
+++ src/CodeEditor/index.tsx
@@ -12,32 +12,34 @@ import { highlightCode } from './utils/highlightCode';
 interface ICodeEditorProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
   value: string;
   onChange(code: string): any;
+  padding?: number | string;
   language?: string;
   autoFocus?: boolean;
 }
 
-const CodeEditor: React.FunctionComponent<ICodeEditorProps> = React.forwardRef<HTMLDivElement, ICodeEditorProps>(
-  function Editor(props, ref) {
-    const { autoFocus, language, onChange, value, placeholder, className, ...rest } = props;
+const CodeEditor = React.forwardRef<ReactSimpleCodeEditor, ICodeEditorProps>((props, ref) => {
+  const { autoFocus, language, onChange, value, placeholder, className, padding, style, ...rest } = props;
 
-    // Highlight code on change
-    const highlight = React.useCallback(code => (language ? highlightCode(code, language) : code), [language]);
+  // Highlight code on change
+  const highlight = React.useCallback(code => (language ? highlightCode(code, language) : code), [language]);
 
-    return (
-      <div className={cn('bp3-code-editor relative', className)} {...rest}>
-        {!value && placeholder && <span className="absolute text-disabled">{placeholder}</span>}
-        <ReactSimpleCodeEditor
-          autoFocus={autoFocus}
-          // @ts-ignore FIXME type erorr
-          ref={ref}
-          value={value}
-          onValueChange={onChange}
-          highlight={highlight}
-        />
-      </div>
-    );
-  }
-);
+  return (
+    <ReactSimpleCodeEditor
+      {...rest}
+      // @ts-ignore FIXME type error
+      ref={ref}
+      className={cn(className, 'bp3-code-editor')}
+      style={style}
+      placeholder={placeholder}
+      autoFocus={autoFocus}
+      value={value}
+      onValueChange={onChange}
+      highlight={highlight}
+      padding={padding}
+    />
+  );
+});
+CodeEditor.displayName = 'CodeEditor';
 
 /**
  * EXPORTS
