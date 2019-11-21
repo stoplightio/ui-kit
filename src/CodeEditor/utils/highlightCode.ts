acf37800e670d9e0587ecb90db69fe71d36788c7
Sarah Spear <sarahrva@gmail.com>
2019-11-21 10:35:33 -0600

    chore: update highlightCode styles

--- src/CodeEditor/utils/highlightCode.ts
+++ src/CodeEditor/utils/highlightCode.ts
@@ -1,11 +1,22 @@
 import * as Prism from 'prismjs';
 
 import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-go';
 import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-objectivec';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-swift';
 import 'prismjs/components/prism-yaml';
 
 export const highlightCode = (code: string = '', language: string, showLineNumbers?: boolean) => {
