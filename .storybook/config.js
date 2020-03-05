8499db1cf5082e04856027ff9b913c2ae6c02332
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-03-05 16:50:23 -0600

    chore: use @stoplight/storybook-config (#141)
    # Conflicts:
    #	package.json
    #	yarn.lock

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,30 +1,3 @@
-import * as React from 'react';
-import { withOptions } from '@storybook/addon-options';
-import { addDecorator, configure } from '@storybook/react';
+import '@stoplight/storybook-config/config';
 
 import '../src/styles/_ui-kit.scss';
-import { ThemeContainer } from '../src/';
-
-addDecorator(
-  withOptions({
-    name: 'Stoplight UI-Kit',
-    url: 'https://github.com/stoplightio/ui-kit',
-    goFullScreen: false,
-    showStoriesPanel: true,
-    showAddonPanel: true,
-    showSearchBox: false,
-    addonPanelInRight: true,
-    sortStoriesByKind: true,
-    hierarchySeparator: /\//,
-    hierarchyRootSeparator: /:/,
-    selectedAddonPanel: undefined,
-  })
-);
-
-function loadStories() {
-  require('@project/stories');
-}
-
-addDecorator(story => <ThemeContainer>{story()}</ThemeContainer>);
-
-configure(loadStories, module);
