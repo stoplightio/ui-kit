9d725876afeab3249e8489cefa903a2817fd179c
Marcell Toth <marcell@marcelltoth.net>
2020-04-07 18:04:48 +0300

    perf: minor rerender optimization

--- src/_hooks/useValidateSchema.ts
+++ src/_hooks/useValidateSchema.ts
@@ -2,27 +2,30 @@ import { isEqual } from 'lodash';
 import * as React from 'react';
 import * as yup from 'yup';
 
+const noError: string[] = [];
+const unknownError = ['Input is invalid'];
+
 export function useValidateSchema(
   schema?: yup.Schema<any>,
   value?: any,
   { abortEarly, recursive, context }: yup.ValidateOptions = {},
 ): string[] {
-  const [errors, setErrors] = React.useState<string[]>([]);
+  const [errors, setErrors] = React.useState<string[]>(noError);
   const memoizedSchema = useDeepCompareMemoize(schema);
   const memoizedContext = useDeepCompareMemoize(context);
 
   React.useEffect(() => {
     if (!memoizedSchema) {
-      setErrors([]);
+      setErrors(noError);
       return;
     }
     memoizedSchema
       .validate(value, { strict: true, abortEarly, recursive, context: memoizedContext }) // to avoid taking a useEffect dependency on validateOpts that is an object
       .then(() => {
-        setErrors([]);
+        setErrors(noError);
       })
       .catch(e => {
-        setErrors(e.errors || ['Input is invalid']);
+        setErrors(e.errors || unknownError);
       });
   }, [memoizedSchema, value, abortEarly, recursive, memoizedContext]);
 
