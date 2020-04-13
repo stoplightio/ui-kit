24fbd2c8790a2ee74cb02d38909d044a5d210f35
Marc MacLeod <marc@stoplight.io>
2020-04-13 00:38:33 -0500

    chore: fix storybook issue

--- src/__stories__/Code/CodeViewer.tsx
+++ src/__stories__/Code/CodeViewer.tsx
@@ -5,14 +5,40 @@ import * as React from 'react';
 import { CodeViewer, ICodeViewerProps } from '../../CodeViewer';
 
 export const codeViewerKnobs = (tabName = 'Code Viewer'): ICodeViewerProps => ({
-  language: text('language', 'javascript', tabName),
-  value: text('value', 'const defaultValue = stoplight.io();', tabName),
+  language: text('language', 'jsx', tabName),
+
+  value: text(
+    'value',
+    `function Clock(props) {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    var timerID = setInterval(() => tick(), 1000);
+    function defaultValue = stoplight.io();
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
+  return (
+    <div>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}`,
+    tabName,
+  ),
+
   inline: boolean('inline', false, tabName),
   showLineNumbers: boolean('showLineNumbers', false, tabName),
 });
 
 storiesOf('Code:Viewer', module)
-  .addDecorator(withKnobs)
+  .addDecorator(withKnobs({ escapeHTML: false }))
   .add('with defaults', () => <CodeViewer {...codeViewerKnobs()} />)
   .add('dark', () => (
     <div className="bp3-dark">
