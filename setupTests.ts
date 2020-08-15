3a3a89429ad0c0068f13c80346f14a9119e3e408
Jakub Rożek <jakub@stoplight.io>
2020-08-15 20:54:40 +0200

    test: mock IntersectionObserver

--- setupTests.ts
+++ setupTests.ts
@@ -5,3 +5,14 @@ Enzyme.configure({ adapter: new Adapter() });
 jest.mock('lodash');
 jest.mock('lodash/debounce');
 jest.mock('react-window');
+
+window.IntersectionObserver = class implements IntersectionObserver {
+  readonly root!: Element | null;
+  readonly rootMargin!: string;
+  readonly thresholds!: ReadonlyArray<number>;
+
+  public readonly observe = jest.fn();
+  public readonly unobserve = jest.fn();
+  public readonly disconnect = jest.fn();
+  public readonly takeRecords = jest.fn();
+};
